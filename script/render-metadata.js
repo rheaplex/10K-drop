525bde3dda6aac3a341ffd5ca753d22fa4bfc717
Rhea Myers <rhea@myers.studio>
2024-07-12 16:00:43 -0700

    Start IDs at 0, change title generation to make this less confusing.

--- script/render-metadata.js
+++ script/render-metadata.js
@@ -55,7 +55,7 @@ for (let i = 0; i < config.edition; i++) {
       {
         "image": ipfs(`./dist/image/${id}`),
         "animation_url": `${animationDirHash}/${id}`,
-        "name": `${config.projectName} #${id}`,
+        "name": `${config.projectName} ${id}`,
         "description": config.projectDescription,
         "svg_url": ipfs(`./dist/svg/${id}`)
       },
--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@ const projectName        = "10K Drop";
 const projectDescription = "A 10K drop that you can own as an NFT. Locked until ETH hits 10KUSD.";
 
 const edition            = 200;
-const firstId            = 1;
+const firstId            = 0;
 
 const width              = 7680;
 const height             = 4320;
